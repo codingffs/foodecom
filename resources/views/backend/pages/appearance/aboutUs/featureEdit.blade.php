bf572811502eb79dd92135bbd28385a16f6dba19
urmi-shakti <urmi.shaktiwebsolutions@gmail.com>
2024-04-08 18:11:20 +0530

    changes

--- resources/views/backend/pages/appearance/aboutUs/featureEdit.blade.php
+++ resources/views/backend/pages/appearance/aboutUs/featureEdit.blade.php
@@ -91,21 +91,6 @@
                     </form>
                 </div>
 
-                <!--right sidebar-->
-                {{-- <div class="col-xl-3 order-1 order-md-1 order-lg-1 order-xl-2">
-                    <div class="card tt-sticky-sidebar">
-                        <div class="card-body">
-                            <h5 class="mb-4">{{ localize('About Us Configuration') }}</h5>
-                            <div class="tt-vertical-step">
-                                <ul class="list-unstyled">
-                                    <li>
-                                        <a href="#section-1" class="active">{{ localize('Update Feature') }}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
 
         </div>
